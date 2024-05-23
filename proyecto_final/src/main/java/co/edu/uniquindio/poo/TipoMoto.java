0c7b5a169adf15601feab1ad6e009efd0d01a07a
Esteban higuita <juane.gutierrezh@uqvirtual.edu.co>
2024-05-22 23:27:02 -0500

    Se agrega la clase tipo moto

---
+++ proyecto_final/src/main/java/co/edu/uniquindio/poo/TipoMoto.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.poo;
+
+public enum TipoMoto {
+    /* 
+     * El enum tipo moto define los tipos de motos que vamos a tener.
+     */
+    MOTOCLASICA,
+    MOTOHIBRIDA;
+
+    
+}
